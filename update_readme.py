dd5103fc7ddc8c03d91a64bc8b93c76201b85253
w4zu <dkevin@kokiris.com>
2025-02-12 19:07:47 +0100

    modif indent readme

--- update_readme.py
+++ update_readme.py
@@ -162,7 +162,7 @@ def generate_readme(alerts):
                 readme_content += f"- **[{cve}](https://www.cve.org/CVERecord?id={cve})** : {severity} (Score: {score if score else 'N/A'})\n\n"
             readme_content += f"**Debian Version :** {alert['suite']}\n "
             readme_content += f"**Package Version :** {alert['version']}\n "
-            readme_content += f"**Type :** {alert['type']}\n "
+            readme_content += f"**Type :** {alert['type']}\n\n"
     else:
         readme_content += "No alerts found for the last 14 days.\n\n"
 
